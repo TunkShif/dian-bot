93c60bd8faae389bb52283a3be354c1b1eb97579
TunkShif <tunkshif@foxmail.com>
2023-12-22 22:17:24 +0800

    update: wrapped statistics function

--- app/assets/js/utils/client.ts
+++ app/assets/js/utils/client.ts
@@ -2,6 +2,7 @@ import { csrfToken } from "@/session"
 import { QueryClient } from "@tanstack/react-query"
 import ky from "ky"
 
+// TODO: cache (persistent cache/service worker cache)
 export const queryClient = new QueryClient()
 
 export const httpClient = ky.create({
