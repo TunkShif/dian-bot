fed4d7b9c3408e3e2081ac8b6ef5c60bc00dd498
TunkShif <tunkshif@foxmail.com>
2024-01-29 11:54:21 +0800

    (server:coucou)/refactor: colocate parser module

--- server/lib/dian_bot/coucou.ex
+++ server/lib/dian_bot/coucou.ex
@@ -3,6 +3,45 @@ defmodule DianBot.Coucou do
   Message parser with CQ code support.
   """
 
+  defmodule Parser do
+    @moduledoc false
+
+    import NimbleParsec
+
+    comma = string(",")
+    equal = string("=")
+    closing = string("]")
+
+    ident = ascii_string([?a..?z, ?A..?Z], min: 1)
+
+    pair = ident |> ignore(equal) |> concat(utf8_string([not: ?,, not: ?]], min: 1)) |> wrap()
+
+    pairs =
+      choice([pair, ignore(comma)])
+      |> repeat()
+      |> map({List, :to_tuple, []})
+      |> reduce({Map, :new, []})
+
+    type = unwrap_and_tag(ident, :type)
+    data = unwrap_and_tag(pairs, :data)
+
+    code =
+      ignore(string("[CQ:"))
+      |> concat(type)
+      |> ignore(comma)
+      |> concat(data)
+      |> concat(ignore(closing))
+      |> reduce({Map, :new, []})
+
+    text = utf8_string([{:not, ?[}, {:not, ?]}], min: 1) |> reduce({:wrap_text, []})
+
+    message = choice([code, text]) |> repeat()
+
+    defparsec(:parse, message)
+
+    defp wrap_text([text]), do: %{type: "text", data: text}
+  end
+
   @doc ~S"""
   Parse a message text containing CQ code.
 
@@ -11,7 +50,7 @@ defmodule DianBot.Coucou do
       [%{type: "at", data: %{"qq" => "8964"}}, %{type: "text", data: "foobar"}]
   """
   def parse_message(source) do
-    {:ok, result, _rest, _context, _position, _byte_offset} = DianBot.Coucou.Parser.parse(source)
+    {:ok, result, _rest, _context, _position, _byte_offset} = Parser.parse(source)
     result
   end
 end
